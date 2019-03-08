b6b94649df06593bcf03da1f9dbb0ab4c439c0b0
joy <wendokingoo@gmail.com>
2019-03-08 11:14:09 +0300

    added save method in image model

--- jgram/models.py
+++ jgram/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
+
+
+
 class Image(models.Model):
     name = models.CharField(max_length=60)
     image = models.ImageField(upload_to="image/")
-    description = models.TextField()
+    comments= models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def save_image(self):
+        self.save()
