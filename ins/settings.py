8fbf81466247e01c72c13b230bea8cbd8b0c7ae4
joy <wendokingoo@gmail.com>
2019-03-08 10:48:08 +0300

    sign up and login modified

--- ins/settings.py
+++ ins/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,9 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ')%kpwmw-5k!t-mdbq$i$w-$1d-5@a^f1_(!$wkkgx%!r9q=8ag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -74,8 +79,10 @@ WSGI_APPLICATION = 'ins.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'joygram',
+        'USER': 'joy',
+    'PASSWORD':'joy',
     }
 }
 
