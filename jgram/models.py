189cc696969ba83f180272ba8686accf1b21a440
joy <wendokingoo@gmail.com>
2019-03-08 15:14:36 +0300

    Models updated

--- jgram/models.py
+++ jgram/models.py
@@ -2,8 +2,6 @@ from django.db import models
 import datetime as dt
 # Create your models here.
 
-
-
 class Image(models.Model):
     name = models.CharField(max_length=60)
     image = models.ImageField(upload_to="image/")
@@ -12,3 +10,22 @@ class Image(models.Model):
 
     def save_image(self):
         self.save()
+
+class Followers(models.Model):
+    name = models.CharField(max_length=60)
+
+    def save_followers(self):
+        self.save()
+
+
+class Profile(models.Model):
+    user = models.CharField(max_length=60)
+    bio =models.TextField()
+    image=models.ForeignKey(Image)
+    followers=models.ForeignKey(Followers)
+    
+    
+    
+    def save_profile(self):
+        self.save()
+
